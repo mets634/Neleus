44b5d5478f0c5502104895f68f4f081f1756af0c
root <root@localhost.localdomain>
2017-06-19 16:18:03 +0300

    Reorganized files

---
+++ config/services/ConfigDns64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+name="ConfigDns64"
+
+# assumptions:
+# 1) DNS will use interface eth0.
+# 2) Device has an external DNS server to access.
+
+
+if ! hash bind  2>/dev/null; then
+    echo "[$name]Bind9 not found. Installing module..."
+    apt-get install bind9
+fi
+
+echo "[$name]Writing DNS64 config file to BIND9..."
+echo "options {
+    directory \"/var/cache/bind\";
+    auth-nxdomain no;
+    listen-on-v6 { any; };
+    allow-query { any; };
+    dns64 6d65:7473:3633:3400::/96 {
+        clients { any; };
+    };
+};" > /etc/bind/named.conf.options
+
+echo "[$name]Restarting DNS server..."
+service bind9 restart
