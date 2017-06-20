40761184844e483cf2821e2527054e3e3ddda937
root <root@localhost.localdomain>
2017-06-20 14:11:40 +0300

    Trying to allow onlink

--- config/services/ConfigRadvd.sh
+++ config/services/ConfigRadvd.sh
@@ -24,7 +24,7 @@ echo "interface eth0 {
 
     prefix 6D65:7473:3633:3400::/64 
     {
-        AdvOnLink off;
+        AdvOnLink on;
         AdvAutonomous on; # clients should use SLAAC auto-configuration
         AdvRouterAddr on;
     };
