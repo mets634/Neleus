fa0a7f02aa156dd15498dddf7b4376db7e8c787a
root <mets634@gmail.com>
2017-06-21 18:46:05 +0300

    YAYYgit add .! Need to extend DNS64

--- config/test.sh
+++ config/test.sh
@@ -3,3 +3,7 @@
 ./services/ConfigDHCPv6.sh
 ./services/ConfigDns64.sh
 ./services/ConfigRadvd.sh
+./infrastructure/ConfigTayga.sh $1
+tail /var/log/syslog
+tail /var/log/messages
+atk6-parasite6 eth0
